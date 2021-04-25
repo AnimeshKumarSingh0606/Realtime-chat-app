df9842c90248b2fa45909f1b1f840ce90b9b6900
Animesh Kumar Singh <animesh.2001singh@gmail.com>
2021-04-25 14:39:55 +0530

    chat app is working absolutely  awesome

---
+++ client/build/precache-manifest.daef63b3c57e2955fbafca3b68c4f991.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc160db4bbb6399ae567c5a5f281cef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4022da8f21247ae5be5",
+    "url": "/static/css/main.98346d44.chunk.css"
+  },
+  {
+    "revision": "70b83e14ee913fd16fa8",
+    "url": "/static/js/2.aa705c09.chunk.js"
+  },
+  {
+    "revision": "e4022da8f21247ae5be5",
+    "url": "/static/js/main.e5af9468.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
